2e750604aaf95426e41134f1144ab1528950e00c
González Ramón Jesus <jesus.gonzalez@municipio.merida.gob.mx>
2023-01-17 09:14:54 -0600

    Que es otex avances

--- src/components/principal.jsx
+++ src/components/principal.jsx
@@ -19,60 +19,52 @@ const Principal = () => {
             </div>
 
             {/* Que es Otex */}
-            <div className="principal d-flex flex-column justify-content-between">
-                <div className="container-fluid w-14">
-                    <div className="row">
-                        <div className="col-12 col-md-6 col-xl-5 mb-4 text-center text-md-left">
-                            <h1 className="titulos">WHAT’S OTEX</h1>
-                        </div>        
-                        <div className="col-12 col-md-6 col-xl-7 pl-xl-5 text-justify">
-                            <p className="textos mb-3">OTEX helps decentralized exchanges execute asset swaps at the best possible prices — pulling liquidity across different liquidity providers —, for the lowest possible cost — reducing gas consumption wherever possible — and in the safest way possible — protecting from MEV.</p>
-                            <p className="textos">The stack of proprietary on-chain and off-chain software we currently maintain and are integrating across different DeFi protocols are:</p>
-                        </div>
-                    </div>
+            <div className="principal p-0 min-h-auto">
+                <div className="container-fluid w-14 h-100">
+					<div className="min-h-height d-flex justify-content-between flex-column h-100 py-40">
+						{/* Info */}
+						<div className="row">
+							<div className="col-12 col-md-6 col-xl-5 mb-4 text-center text-md-left">
+								<h1 className="titulos">WHAT’S OTEX</h1>
+							</div>        
+							<div className="col-12 col-md-6 col-xl-7 pl-xl-5 text-justify">
+								<p className="textos mb-3">OTEX helps decentralized exchanges execute asset swaps at the best possible prices — pulling liquidity across different liquidity providers —, for the lowest possible cost — reducing gas consumption wherever possible — and in the safest way possible — protecting from MEV.</p>
+								<p className="textos">The stack of proprietary on-chain and off-chain software we currently maintain and are integrating across different DeFi protocols are:</p>
+							</div>
+						</div>
 
-                    <div className="row">
-                        <div className="col-12">
-                            <div className="row" style={{paddingTop:"70px",paddingBottom:"70px"}}>
-                                <div className="col-12 col-md-4 col-xs-6 text-center">
-                                    <div className="card card-principal h-100">
-                                        
+						{/* Cards */}
+						<div className="row text-center">
+							<div className="col-12 col-md-4 col-xs-6 ">
+								<div className="wrapper p-3 d-flex align-items-center justify-content-center">
+									<p className="textos">
+										In-house built DEX aggregators that address bespoke price discovery requirements imposed by different decentralized exchanges.
+									</p>
+								</div>
+							</div>
+							<div className="col-12 col-md-4 col-xs-6 text-center">
+								<div className="wrapper p-3 d-flex align-items-center justify-content-center">
+									<p className="textos">
+										Market making RFQ systems that underwrite just-in-time (deep) liquidity.
+									</p>
+								</div>
+							</div>
+							<div className="col-12 col-md-4 col-xs-6 text-center">
+								<div className="wrapper p-3 d-flex align-items-center justify-content-center">
+									<p className="textos">
+										Gas-optimized settlement execution contracts
+									</p>
+								</div>
+							</div>
+						</div>
 
-                                        <div className="p-3">
-                                            
-                                            <p className="title-principal">In-house built DEX aggregators that address bespoke price discovery requirements imposed by different decentralized exchanges.</p>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="col-12 col-md-4 col-xs-6 text-center">
-                                    <div className="card card-principal h-100">
-                                        
-
-                                        <div className="p-3">
-                                            
-                                            <p className="title-principal">Market making RFQ systems that underwrite just-in-time (deep) liquidity.</p>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="col-12 col-md-4 col-xs-6 text-center">
-                                    <div className="card card-principal h-100">
-                                        
-
-                                        <div className="p-3">
-                                            
-                                            <p className="title-principal">Gas-optimized settlement execution contracts.</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                    
-
-                        <div className="text-center">
-                            <p className="title-principal">Our systems have helped settled <b style={{color:"#AF48FF"}}>+1B USD</b> of on-chain transactions. <br />  We are keen to speak with new and established protocols to explore the <br /> possibility of integrating our infrastructure into your ecosystem.</p>
-                        </div>
-                    </div>
+						{/* Text */}
+						<div className="row justify-content-center text-center">
+							<div className="col-12 col-md-9 col-lg-8 col-xl-7">
+								<p className="title-principal">Our systems have helped settled <b style={{color:"#AF48FF"}}>+1B USD</b> of on-chain transactions. <br />  We are keen to speak with new and established protocols to explore the <br /> possibility of integrating our infrastructure into your ecosystem.</p>
+							</div>
+						</div>
+					</div>
                 
                     <NextSeccion theme={'white'} />
                 </div>
