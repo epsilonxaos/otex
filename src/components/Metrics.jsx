ae1f4905681ecdc9c80835a1fe818a408b584805
González Ramón Jesus <jesus.gonzalez@municipio.merida.gob.mx>
2023-01-17 15:59:06 -0600

    Avances en el coount up

--- src/components/Metrics.jsx
+++ src/components/Metrics.jsx
@@ -1,8 +1,17 @@
 import NextSeccion from "./nextSeccion";
+import CountUp from "react-countup";
+import { useCountUp } from 'react-countup';
+import { useInView } from 'react-intersection-observer';
 
 const Metrics = () => {
+	const { ref, inView, entry } = useInView({
+		/* Optional options */
+		threshold: 0,
+		triggerOnce: true
+	});
+
 	return (
-		<div className="panel-pin  min-h-screen" data-theme="white">
+		<div className="panel-pin min-h-height" data-theme="white" ref={ref}>
 			<div className="metrics d-flex flex-column justify-content-between">
 				<div className="container-fluid w-14" style={{paddingBottom:"80px"}}>
 					<div className="row justify-content-center" >
@@ -11,13 +20,16 @@ const Metrics = () => {
 								<h1 className="titulos">Metrics</h1>
 								<img src="src/assets/img/flechametrics.svg" alt="" />
 							</div>
-							<div className="row metrics-card"  >
+
+							
+
+							<div className="row metrics-card" >
 								<div className="col-12 col-md-4 rect-metrics text-center" >
 									<p className="titulos">+1B</p>
 									<p className="textos">VOLUME SETTLED</p>
 								</div>
 								<div className="col-12 col-md-4 rect-metrics text-center">
-									<p className="titulos">+10,000</p>
+									<p className="titulos">+ {inView ? <CountUp start={0} end={10000} delay={0}> {({ countUpRef, }) => {console.log(countUpRef); return(<div> <span ref={countUpRef} /> </div> )}} </CountUp> : '10,000'}</p>
 									<p className="textos">TRADE OPTIMISED</p>
 								</div>
 								<div className="col-12 col-md-4 rect-metrics text-center">
@@ -25,6 +37,7 @@ const Metrics = () => {
 									<p className="textos">INTEGRATED PROTOCOLS</p>
 								</div>
 							</div>
+
 						</div>
 					</div>
 				</div>
@@ -32,6 +45,16 @@ const Metrics = () => {
 			</div>
 		</div>
 	);
+};
+
+function numberWithCommas(num) {
+	console.log(num);
+	// return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
+const SimpleHook = ({endNumber}) => {
+	const { countUp } = useCountUp({ start: 0, end: endNumber });
+	return <div>{numberWithCommas(countUp)}</div>;
 };
 
 export default Metrics;
