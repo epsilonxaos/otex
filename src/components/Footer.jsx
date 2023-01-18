e76426582c42efd9c01818916091742f987dd03a
johan2507 <johanssin@hotmail.com>
2023-01-17 21:45:09 -0600

    Responsive Metrics - Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,11 +2,12 @@ import bgfooter from '../assets/img/footerbg.svg';
 import { BsDiscord, BsLinkedin, BsTelegram } from "react-icons/bs";
 const Footer = () => {
 	return (
-		<div className="panel-pin min-h-screen" data-theme="white">
-            <div className="footer d-flex flex-column justify-content-between">
-                <div className="container-fluid w-14">
-                    <div className="row justify-content-center">
-                        <div className="col-12 footer-bg text-center " style={{backgroundImage:`url(${bgfooter })`,height:"450px",borderRadius:"16px"}}>
+		<div className="panel-pin footer" data-theme="white">
+            <div className="container-fluid w-14">
+                <div className="d-flex flex-column justify-content-between">
+                    
+                    
+                        {/* <div className="col-12 footer-bg  " style={{backgroundImage:`url(${bgfooter })`,height:"450px",borderRadius:"16px"}}>
                             <p className='textos'>
                                 “OTEX is defying legacy web2 and web3 trade execution middleware <br />
                                 by combining the best of both worlds”
@@ -16,12 +17,13 @@ const Footer = () => {
                             </p>
                             <p className='t-founder'>
                             Founder</p>
-                        </div>
+                        </div> */}
+                        
                         <div className="col-12 justify-content-center " style={{paddingTop:"50px",paddingBottom:"50px"}}>
-                            <ul className="d-flex justify-content-center p-0 list-unstyled m-0 socialMedia">
-                                <li className="col-12 col-md-4 title-footer-buttons centrar footer-botones"><a> <BsLinkedin className='svgFooter'/>  Linkedin</a></li>
-                                <li className="col-12 col-md-4 title-footer-buttons centrar footer-botones"><a> <BsDiscord className='svgFooter'/> Discord</a></li>
-                                <li className="col-12 col-md-4 title-footer-buttons centrar footer-botones"><a> <BsTelegram className='svgFooter'/> Telegram</a></li>
+                            <ul className="centro p-0 list-unstyled m-0 socialMedia">
+                                <li className="col-12 col-md-4 col-xs-6  title-footer-buttons footer-botones"><a> <BsLinkedin className='svgFooter'/>  Linkedin</a></li>
+                                <li className="col-12 col-md-4 col-xs-6  title-footer-buttons footer-botones"><a> <BsDiscord className='svgFooter'/> Discord</a></li>
+                                <li className="col-12 col-md-4 col-xs-6  title-footer-buttons footer-botones"><a> <BsTelegram className='svgFooter'/> Telegram</a></li>
                             </ul>
                         </div>
                         <div className="text-center ">
@@ -31,7 +33,7 @@ const Footer = () => {
                             </p>
                             
                         </div>
-                    </div>
+                    
                 </div>
             </div>
         </div>
