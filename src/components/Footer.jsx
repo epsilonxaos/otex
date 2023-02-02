e68748fe05a228272f0fba8de4f2422c40d8b310
Jesus Glez <jesusgleztr94@gmail.com>
2023-02-01 21:17:00 -0600

    Ajustes en escritorio

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,15 +2,15 @@ import bgfooter from '../assets/img/footerbg.svg';
 import { BsDiscord, BsLinkedin, BsTelegram } from "react-icons/bs";
 const Footer = () => {
 	return (
-		<div className="panel-pin footer pt-5" data-theme="black" id="contact">
+		<div className="panel-pin footer pt-5 pb-0" data-theme="black" id="contact">
             <div className="container-fluid w-14">
                 <div className="d-flex flex-column justify-content-around py-40 pb-2 min-h-height">
 
                    <div className="row">
                         <div className="col-md-12">
                             <div className='footer-bg medio' style={{backgroundImage:`url(${bgfooter })`}}>
-                            <p className='textos mb-0'> “OTEX is defying legacy web2 and web3 trade execution middleware <br />by combining the best of both worlds” </p>
-                            <p className='textos mb-0' style={{paddingTop:"30px"}}> Félix Faria </p>
+                            <p className='textos mb-0'> “OTEX is defying legacy web2 and web3 trade execution middleware <br /> by combining the best of both worlds” </p>
+                            <p className='textos mb-0' style={{paddingTop:"30px"}}> Félix Farías </p>
                             <p className='t-founder tlight'> Founder</p>
                             </div>
                         </div>
